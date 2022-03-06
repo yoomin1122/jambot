b367545d09d45493c56e93131dadae86a21b54e4
yoomin1122 <79196965+yoomin1122@users.noreply.github.com>
2022-03-06 11:37:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from itertools import cycle
 import random
 from discord.ext.commands import CommandNotFound
 import pytz
-from webserver import keep_alive
 from koreanbots.integrations.discord import DiscordpyKoreanbots
 import traceback
+import jishaku
+from discord_slash import SlashCommand
 
 
 bot = commands.Bot(command_prefix="잼민아 ")
 bot.remove_command("help")
-kb = DiscordpyKoreanbots(bot, 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkyMTQyNDcyNDcyOTM5NzMxOCIsImlhdCI6MTY0MDA5ODk2NX0.M_rzLn9UPpo0jpR2HZhs5uBLwtqWdCIHLIIF_-PXZoXbhmDPscEjrYzzCsX7w_6TG7zlKqORD4dyzUNefUXRzxPOA7tWvw8iJFqeW58hyzkR6pR2Igs-FhQ8wvcuBs1MtPdA9GqWhap5E96nSsuXCpoh6tZA9u-GjKakHwnZGc4', run_task=True)
+bot.load_extension('jishaku')
+slash = SlashCommand(bot, sync_commands=True)
+kb = DiscordpyKoreanbots(bot, 'koreanlist_token', run_task=True)
 
-for filename in os.listdir("Cogs"):
+for filename in os.listdir('Cogs'):
     if filename.endswith(".py"):
         bot.load_extension(f"Cogs.{filename[:-3]}")
         print(f"{filename[:-3]}가 로드되었습니다.")
@@ -23,7 +26,7 @@ for filename in os.listdir("Cogs"):
 
 @bot.event
 async def on_ready():
-    status = cycle(["잼민이봇", "잼민아 명령어", str(len(bot.guilds)) + "서버에 잼민이봇이 있음!"])
+    status = cycle(["잼민이봇", "잼민아 명령어", f"{len(bot.guilds)} 서버에 잼민이봇이 있음!"])
 
     @tasks.loop(seconds=8)
     async def change_status():
@@ -44,7 +47,12 @@ async def on_guild_join(guild):
           embed.add_field(name="잼민이봇 투표해주세요!", value="[[투표하기]](https://koreanbots.dev/bots/921424724729397318/vote)", inline=False)
           embed.add_field(name="잼민이봇 초대링크", value="[[초대하기]](http://invite.jambot.kro.kr)")
           await channel.send(embed=embed)
+          print(f"{guild.name}({guild.id})에 추가됨 현재 {len(bot.guilds)}서버에 있음")
         break
+
+@bot.event
+async def on_guild_remove(guild):
+    print(f"{guild.name}({guild.id})에서 추방당함 현재 {len(bot.guilds)}서버에 있음")
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -52,8 +60,7 @@ async def on_command_error(ctx, error):
     err = [line.rstrip() for line in tb]
     errstr = '\n'.join(err)
     if isinstance(error, commands.NotOwner):
-        await ctx.send('봇 주인만 사용 가능한 명령어입니다')
-    elif isinstance(error, CommandNotFound): 
-      await ctx.send("멀아는걷야;; `잼민아 명령어`쳐서 좀 아라와!  \n오류가 계속된다면 `잼민아 버그`명령어를 사용해주세요!")
+        await ctx.send('봇 주인만 사용 가능한 명령어입니다.')   
 
-bot.run(TOKEN)
+
+bot.run('token')
